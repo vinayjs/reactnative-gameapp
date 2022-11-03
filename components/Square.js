c24b1cea7fc3815a492de47dab31d983bb203a84
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-11-03 13:35:50 +0530

    build square component

--- components/Square.js
+++ components/Square.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import { View, StyleSheet  } from 'react-native'
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text  } from 'react-native'
 
 
 
 const Square = () => {
+    const [moleActive, setMoleActive] = useState(false)
+    const[isGameOver, setGameOver] = useState(false)
+    const randomTime = Math.round() * 20000
+    let timerId
+
+    useEffect(()=> {
+        const timerId = setInterval(()=>{
+            setMoleActive(true)
+            setTimeout(() => {setMoleActive(false)}, 800)
+        }, randomTime)
+        setTimeout(endGame, 10 * 1000)
+
+    }, [])
+    
+    function endGame(){
+        clearInterval(timerId)
+        setGameOver(true)
+    }
+
     return (
-        <View style={styles.square}></View>
+        <View style={moleActive? styles.mole :styles.square}>
+        {isGameOver && <Text>X</Text>}
+        </View>
+
     )
 
 }
@@ -17,7 +39,15 @@ const styles = StyleSheet.create({
         minHeight:80,
         margin: 10,
         backgroundColor: 'red'
+    },
+    mole :{
+        flex: 1,
+        minWidth: 80,
+        minHeight:80,
+        margin: 10,
+        backgroundColor: 'blue'
     }
+    
 })
 
 export default Square
